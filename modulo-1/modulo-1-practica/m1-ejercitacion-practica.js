52e95a0817a7dbb594e9179c836f6495b218c3f4
Lucas García <lucasgarcia.developer@gmail.com>
2023-05-30 20:26:06 -0300

    modulo1 - práctica 1

---
+++ modulo-1/modulo-1-practica/m1-ejercitacion-practica.js
@@ -0,0 +1,13 @@
+/**
+ * Ejercicio 1: Mostrar números por consola
+ * Solicitar al usuario un número y mostrar en la consola todos los números desde 1 hasta el número ingresado.
+ */
+let numero = parseInt(prompt("Ingrese un número entero postivo"));
+
+for(let i = 1; i <= numero; i++) {
+    console.log(i)
+}
+
+/**
+ * 
+ */
